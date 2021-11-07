fd7d2e7a5ec869a273b6a491cd5c230241bfb369
Dmitry Kostyuk <dima.kostyuk1508@gmail.com>
2021-11-08 01:19:47 +0300

    Checked

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let shoppingList = [
-
+    // Такие массивы объектов обычно записывают без пустых строк между }, и {
+        
     {
         name: "Banana",
         requiredQuantity: 5,
@@ -26,9 +27,15 @@ let shoppingList = [
     }
 ];
 
+
+// Вы можете сделать эти функции более универсальными, если они будут принимать параметр массив объектов покупок, а не обращаться к нему, как к внешнему массиву. Т.о. ваши функции смогут работать не только с одни единственным массивом, а с каким угодно подобными по структуре массивами объектов покупок.
+
+
 function showShoppingList() {
     for (let elem of shoppingList) {
+
         console.log(elem);
+        // Для целей разработки такого вывода будет более чем достаточно. Однако язык JavaScript используется для Frontend'а, который заключается в выводе данных окончательным пользователям, для которых необходимы человеко-читаемые пояснительные и уточняющие слова-формулировки вместе с выводом данных. В следующих работах реализуйте выводы именно для пользователей.
     }
 }
 
@@ -39,16 +46,21 @@ function sortList() {
 
 function addToList(productName, amountOfProduct) {
     for (let i = 0; i <= shoppingList.length - 1; i++) {
+        // Если использовать оператор "меньше чем <", то можно не выполнять операцию вычитания 1 из shoppingList.length
+
         if (shoppingList[i].name.includes(productName)) {
             shoppingList[i].requiredQuantity += amountOfProduct;
             return;
         } 
     }
+    // Пустая строка
+
     shoppingList.push({
         name: productName,
         requiredQuantity: amountOfProduct,
         isPurchased: false
-        });                   
+        });  
+        //  Закрывающая часть }); должна располагаться на одной вертикали с началом имени массива shoppingList, от которого строится выхов методa push
 }
 
 function makePurchased(productName) {
